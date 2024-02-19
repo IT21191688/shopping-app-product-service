53c3b6e89e609b30098ca7c27c8525087aefb4c8
IT21191688 <IT21191688@my.sliit.lk>
2024-02-19 22:52:13 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, { Application } from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+
+app.use("/", (req, res, next) => {
+  return res.status(200).json({ msg: "Hello From Product" });
+});
+
+app.listen(8001, () => {
+  console.log("Product Microservice Listening to Port 8001");
+});
